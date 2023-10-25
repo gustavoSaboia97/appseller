103180a4dfd7bca4c92a8caa6177eea30bfdc785
gustavosaboia <gustavosaboia97@hotmail.com>
2023-10-25 01:20:23 -0300

    fix: set in memory db for tests

--- src/test/java/br/edu/infnet/appSales/AppSalesApplicationTests.java
+++ src/test/java/br/edu/infnet/appSales/AppSalesApplicationTests.java
@@ -2,12 +2,13 @@ package br.edu.infnet.appSales;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class AppSalesApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
