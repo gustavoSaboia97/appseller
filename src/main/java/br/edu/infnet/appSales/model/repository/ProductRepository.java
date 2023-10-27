cf01adaa629fa7e033fded5c50423837ff90b2ff
gustavosaboia <gustavosaboia97@hotmail.com>
2023-10-26 21:58:29 -0300

    feat: add example to run custom query

--- src/main/java/br/edu/infnet/appSales/model/repository/ProductRepository.java
+++ src/main/java/br/edu/infnet/appSales/model/repository/ProductRepository.java
@@ -2,9 +2,14 @@ package br.edu.infnet.appSales.model.repository;
 
 
 import br.edu.infnet.appSales.model.domain.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
+    @Query("FROM Product p WHERE p.seller.id = :sellerId")
+    Collection<Product> getProducts(Integer sellerId);
 }
