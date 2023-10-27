566fdcb6bc3186a65f0e95b40ee02b68219dff98
gustavosaboia <gustavosaboia97@hotmail.com>
2023-10-26 21:58:02 -0300

    feat: add seller when writing on database

--- src/main/java/br/edu/infnet/appSales/model/domain/factory/ProductAbstractFactory.java
+++ src/main/java/br/edu/infnet/appSales/model/domain/factory/ProductAbstractFactory.java
@@ -1,21 +1,22 @@
 package br.edu.infnet.appSales.model.domain.factory;
 
-import br.edu.infnet.appSales.model.domain.Product;
-import br.edu.infnet.appSales.model.domain.Game;
-import br.edu.infnet.appSales.model.domain.Book;
-import br.edu.infnet.appSales.model.domain.ProductType;
+import br.edu.infnet.appSales.model.domain.*;
 
 import java.math.BigDecimal;
 
 public class ProductAbstractFactory {
     public Product createProductWithTypeAndValues(ProductType productType, String[] values) throws Exception {
+        Seller seller = new Seller.SellerBuilder()
+            .setId(Integer.parseInt(values[1]))
+            .build();
+
         return switch (productType){
-            case ProductType.BOOK -> createBookWithValues(values);
-            case ProductType.GAME -> createGameWithValues(values);
+            case ProductType.BOOK -> createBookWithValuesAndSeller(values, seller);
+            case ProductType.GAME -> createGameWithValues(values, seller);
         };
     }
 
-    public Product createBookWithValues(String[] values){
+    public Product createBookWithValuesAndSeller(String[] values, Seller seller){
         return new Book.BookBuilder()
             .setId(Integer.parseInt(values[0]))
             .setTitle(values[2])
@@ -23,13 +24,14 @@ public class ProductAbstractFactory {
             .setPrice(new BigDecimal(values[4]))
             .setQuantity(Integer.parseInt(values[5]))
             .setType(ProductType.valueOf(values[6]))
+            .setSeller(seller)
             .setAuthorName(values[7])
             .setPublisherName(values[8])
             .setNumberOfPages(Integer.parseInt(values[9]))
             .build();
     }
 
-    public Product createGameWithValues(String[] values){
+    public Product createGameWithValues(String[] values, Seller seller){
         return new Game.GameBuilder()
             .setId(Integer.parseInt(values[0]))
             .setTitle(values[2])
@@ -37,6 +39,7 @@ public class ProductAbstractFactory {
             .setPrice(new BigDecimal(values[4]))
             .setQuantity(Integer.parseInt(values[5]))
             .setType(ProductType.valueOf(values[6]))
+            .setSeller(seller)
             .setPlatformName(values[7])
             .setStudioName(values[8])
             .build();
