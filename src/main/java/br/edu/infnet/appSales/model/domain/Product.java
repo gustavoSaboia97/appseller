0a1eb19994b1b8ff48dfb8a77be52858cb7ce769
gustavosaboia <gustavosaboia97@hotmail.com>
2023-10-31 22:09:29 -0300

    feat: validate basic data

--- src/main/java/br/edu/infnet/appSales/model/domain/Product.java
+++ src/main/java/br/edu/infnet/appSales/model/domain/Product.java
@@ -1,6 +1,11 @@
 package br.edu.infnet.appSales.model.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
 import java.math.BigDecimal;
 
 @Entity
@@ -10,9 +15,15 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @NotNull
+    @Size(min = 10, max = 50)
     private String title;
+    @NotNull
+    @Size(min = 10, max = 100)
     private String description;
+    @Positive
     private BigDecimal price;
+    @PositiveOrZero
     private Integer quantity;
     private ProductType type;
     @ManyToOne
--- src/main/java/br/edu/infnet/appSales/model/domain/Seller.java
+++ src/main/java/br/edu/infnet/appSales/model/domain/Seller.java
@@ -1,6 +1,10 @@
 package br.edu.infnet.appSales.model.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,10 +14,16 @@ public class Seller {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}")
+    @Column(unique = true)
     private String cpf;
+    @NotNull
     private String name;
+    @NotNull
     private String email;
+    @Size(min = 2, max = 10)
     private String nickname;
+    @Column(columnDefinition = "BOOLEAN DEFAULT true")
     private boolean isActive;
     @OneToMany
     @JoinColumn(name = "seller_id")
